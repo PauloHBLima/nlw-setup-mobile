f096f3ea10dc885f72a3eed799631db53d249a26
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2023-01-26 01:05:04 -0300

    CheckBox e comportamento do Text e cor dos hábito,

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -2,7 +2,8 @@ import {TouchableOpacity, Dimensions, TouchableOpacityProps } from "react-native
 import clsx from 'clsx';
 
 
-import { renegateProgressPorcentage } from '../utils/generate-progress-porcentage';
+import { generateProgressPorcentage } from '../utils/generate-progress-porcentage';
+import dayjs from "dayjs";
 
 const WEEK_DAYS = 7;
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
@@ -18,10 +19,21 @@ interface Props extends TouchableOpacityProps {
 
 export function HabitDay({ amountOfHabits = 0, amountCompleted = 0, date, ... rest }: Props) {
 
-    const amountAccomplishedPorcentage = amountOfHabits > 0 ? renegateProgressPorcentage(amountOfHabits, amountCompleted): 0;
+    const amountAccomplishedPorcentage = amountOfHabits > 0 ? generateProgressPorcentage(amountOfHabits, amountCompleted): 0;
+    const today = dayjs().startOf('day').toDate();
+    const isCurrentDay = dayjs(date).isSame(today);
+
     return (
         <TouchableOpacity 
-        className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+        className={clsx("rounded-lg border-2 m-1", {
+           ["bg-zinc-900 border-zinc-800"] : amountAccomplishedPorcentage ===0,
+           ["bg-violet-900 border-violet-700"] : amountAccomplishedPorcentage > 0 &&amountAccomplishedPorcentage < 20,
+           ["bg-violet-800 border-violet-600"] : amountAccomplishedPorcentage > 20 &&amountAccomplishedPorcentage < 40,
+           ["bg-violet-700 border-violet-500"] : amountAccomplishedPorcentage > 40 &&amountAccomplishedPorcentage < 60,
+           ["bg-violet-600 border-violet-500"] : amountAccomplishedPorcentage > 60 &&amountAccomplishedPorcentage < 80,
+           ["bg-violet-500 border-violet-400"] : amountAccomplishedPorcentage > 80,
+           ["border-white border-4"] : isCurrentDay
+        })}
         style={{ width: DAY_SIZE, height: DAY_SIZE }}
         activeOpacity={0.7}
         {...rest}
